404840b66fa66946073d6c82a26d50bf8ef76d10
fabioes99 <fabioes.99@outlook.com>
2023-06-13 15:31:20 -0300

    config

---
+++ web/src/components/Config.tsx
@@ -0,0 +1,75 @@
+import { Check } from "phosphor-react";
+import * as Checkbox from '@radix-ui/react-checkbox';
+import { FormEvent } from "react";
+
+
+export function Config() {  
+
+ async function createNewHabit(event: FormEvent){
+    event.preventDefault()
+
+    alert('Configuracoes atualizadas com sucesso');
+  }
+
+
+  return (
+    <form onSubmit={createNewHabit} className="w-full flex flex-col mt-6" >
+
+    <div className="flex flex-col gap-2 mt-3">
+      
+          <Checkbox.Root 
+            key={1} 
+            className='flex items-center gap-3 group'
+            >
+            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-50 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background'>
+            <Checkbox.Indicator>
+              <Check size={20} className="text-white" />
+            </Checkbox.Indicator>
+            </div>
+                        
+            <span className=' text-white leading-tight '>
+              Aceita Sugestoes
+            </span>
+          </Checkbox.Root>
+
+          <Checkbox.Root 
+            key={1} 
+            className='flex items-center gap-3 group'
+            >
+            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-50 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background'>
+            <Checkbox.Indicator>
+              <Check size={20} className="text-white" />
+            </Checkbox.Indicator>
+            </div>
+                        
+            <span className=' text-white leading-tight '>
+              Permite enviar E-mail
+            </span>
+          </Checkbox.Root>
+
+          <Checkbox.Root 
+            key={1} 
+            className='flex items-center gap-3 group'
+            >
+            <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-50 transition-colors group-focus:ring-2 group-focus:ring-violet-600 group-focus:ring-offset-2 group-focus:ring-offset-background'>
+            <Checkbox.Indicator>
+              <Check size={20} className="text-white" />
+            </Checkbox.Indicator>
+            </div>
+                        
+            <span className=' text-white leading-tight '>
+              Permite Agenda
+            </span>
+          </Checkbox.Root>
+       
+      </div>
+
+
+    
+
+    <button type="submit" className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-400">
+      <Check size={20} weight="bold" />
+      Confirmar</button>
+  </form>
+  )
+}
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -5,6 +5,6 @@ import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+        <App />
+  </React.StrictMode>
 )
